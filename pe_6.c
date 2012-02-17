aeb408c70a1e05c2500caf345af1315d75bdbd5e
Alan Lin <linpd2@gmail.com>
2012-02-17 18:55:23 -0500

    prob 6 solution

---
+++ pe_6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*Question 6
+ *Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+int main(){
+	int i,sum_os, sum_inc, sum_sq, 
+	    num, answer;
+	scanf("%d", &num);
+	sum_os = 0;
+	for(i = 1; i <= num; ++i)
+		sum_os += i*i;
+
+	sum_inc = (num/2)*(num+1);
+	sum_sq = sum_inc * sum_inc;
+	answer = sum_sq - sum_os;
+
+	printf("%d\n", answer);
+}
