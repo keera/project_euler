6b8e2ad6423b5b6430341fc85d7f29be653af86c
Alan Lin <linpd2@gmail.com>
2012-02-19 00:19:42 -0500

    problem 4 solution

--- pe_4.php
+++ pe_4.php
@@ -4,14 +4,13 @@
  */
 $i = 999999;
 while($i > 100001):
-	if (isDiv($i))
-		return $i;
+	if (isDiv($i)): return $i; endif;
 	$i = (substr($i, 2, 2) == "00") ? $i-110 : $i-1100;
 endwhile;
 
 function isDiv($b) {
 	for ($i = 100; $i <= 999; $i++):
-		if ($b % $i == 0 && strlen($b / $i) == 3)
+		if ($b % $i == 0 && strlen($b / $i) == 3) 
 			return TRUE;
 	endfor;
 	return FALSE;
