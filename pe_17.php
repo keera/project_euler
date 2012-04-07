f1f89b8d603335bf9543b57f1704ce1ba7fef0b2
Alan Lin <linpd2@gmail.com>
2012-04-07 19:25:39 -0400

    removed magic num

--- pe_17.php
+++ pe_17.php
@@ -5,6 +5,7 @@
 $ones = array(3,3,5,4,4,3,5,5,4); // 1 - 9
 $teens = array(3,6,6,8,8,7,7,9,8,8); // 10 - 19
 $tys = array(6,6,5,5,5,7,6,6); // 20 - 90
+$one_thousand = 11;
 $sum_ones = array_sum($ones);
 $sum_teens = array_sum($teens);
 //now find all from 20 - 99;
@@ -18,6 +19,6 @@ for($i = 0; $i < sizeof($ones); $i++):
 	$sum_hun += ((($ones[$i]+10) * 100) + $under_100 - 3);
 endfor;
 
-$total = $sum_hun + $under_100 + 11;
+$total = $sum_hun + $under_100 + $one_thousand;
 echo $total;
 ?>
